5fe421ae3066b459d503f5f80bb33fc538e25e38
Arthur <138830298+Arthurnora@users.noreply.github.com>
2024-03-03 23:03:38 +0000

    praticas_media

--- .ccls-cache/@home@runner@APC/prática@media.c
+++ .ccls-cache/@home@runner@APC/prática@media.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
-int main(void) {
-  printf("Hello World\n");
+int main() {
+  float p1 = 1.0;
+  float p2 = 1.0;
+
+  printf("Digite a nota da p1: ");
+  scanf("%f", &p1);
+  printf("Digite a nota da p2:");
+  scanf("%f", &p2);
+
+  float media = (0.4 * p1) + (0.6 * p2);
+
+  printf("A média é: %f\n", media);
+
+  if (p1 < 10 && p1 > 0 && p2 < 10 && p2 > 0) {
+    printf("Notas válidas\n");
+  } else {
+    printf("Notas inválidas, tem que colocar uma nota <10\n");
+  }
   return 0;
 }
