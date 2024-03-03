77eb1854bb6e9ab50997cdc3804dc7d44c82f67a
Arthur <138830298+Arthurnora@users.noreply.github.com>
2024-03-03 23:54:51 +0000

    .
    Comentário triangulo.c

--- praticas/pratica2/triangulo.c
+++ praticas/pratica2/triangulo.c
@@ -1,4 +1,4 @@
-/*Faça um programa em C que calcule a área de um triângulo (a = base x altura /
+/*Faça um programa em C que calcule a área de um triângulo (a = base * altura /
  * 2). Considere base e altura números inteiros maior que 0.*/
 #include "stdio.h"
 int main(void) {
