92aa6658ff6a3b779a01c345d1490e1b1d18acff
Arthur <138830298+Arthurnora@users.noreply.github.com>
2024-03-03 23:19:28 +0000

    .
    Adicionei pratica1

---
+++ pratica/pratica1/media.c
@@ -0,0 +1,25 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+  float p1 = 1.0;
+  float p2 = 1.0;
+
+  printf("Digite a nota da p1: ");
+  scanf("%f", &p1);
+  printf("Digite a nota da p2:");
+  scanf("%f", &p2);
+
+  float media = (0.4 * p1) + (0.6 * p2);
+
+  media = roundf(media * 1000) / 1000;
+
+  printf("A média é: %.2f\n", media);
+
+  if (p1 < 10 && p1 > 0 && p2 < 10 && p2 > 0) {
+    printf("Notas válidas\n");
+  } else {
+    printf("Notas inválidas, tem que colocar uma nota <10\n");
+  }
+  return 0;
+}
