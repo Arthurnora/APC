6631aae6c1dee656f6e4a9e3d9db119d08cba686
Arthur <138830298+Arthurnora@users.noreply.github.com>
2024-03-04 13:31:54 +0000

    .
    modificações na aula2
    novo output de pratica2

--- .ccls-cache/@home@runner@APC/aulas@aula2@tipos.c
+++ .ccls-cache/@home@runner@APC/aulas@aula2@tipos.c
@@ -8,6 +8,17 @@ int main(void) {
   long double decimal18;  // numeros decimais com 18casas
 
 
+
+  signed char caracter_com_sinal; // 1 byte -128 a 127
+  unsigned char caracter_sem_sinal; // 1 byte 0 a 255
+
+  signed int inteiro_com_sinal; // 4 bytes -21b a 21b
+  unsigned int inteiro_sem_sinal; // 4 bytes 0 a 42b
+
+  short int inteiro_curto; // 2 bytes -32768 a 32767
+  long int inteiro_longo; // 8 bytes -9hexa a 9hexa
+
+  long double duplo_longo;  // 16 bytes -1.2E+4932 a 1.2E+4932
   
   return 0;
 }
---
+++ aulas/aula2/variaveis_constantes.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+  const float PI = 3.14f;
+
+
+  
+  return 0;
+}
