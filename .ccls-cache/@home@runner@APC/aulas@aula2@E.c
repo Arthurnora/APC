b7f0b4916cf69abd49a069a72fa9c97247ae5ca6
Arthur <138830298+Arthurnora@users.noreply.github.com>
2024-03-04 12:00:50 +0000

    .
    aula2

--- .ccls-cache/@home@runner@APC/aulas@aula2@E.c
+++ .ccls-cache/@home@runner@APC/aulas@aula2@E.c
@@ -1,4 +1,13 @@
 #include "stdio.h"
 int main(void) {
+  char caracter; // caracteres 'a', 'A'
+  int inteiro;  // numero inteiros
+  float decimal6;  //  numero decimal com 6casas
+  double decimal15;  // numeros decimais com 15casas
+  void;  // vazio
+  long double decimal18;  // numeros decimais com 18casas
+
+
+  
   return 0;
 }
