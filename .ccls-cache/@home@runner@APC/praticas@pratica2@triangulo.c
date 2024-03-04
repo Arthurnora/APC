b5beae169f24f825546c5dab20a21722ecefb8ae
Arthur <138830298+Arthurnora@users.noreply.github.com>
2024-03-04 12:53:57 +0000

    mudanças e remoções

--- .ccls-cache/@home@runner@APC/praticas@pratica2@triangulo.c
+++ .ccls-cache/@home@runner@APC/praticas@pratica2@triangulo.c
@@ -1,21 +1,23 @@
-/*Faça um programa em C que calcule a área de um triângulo (a = base * altura /
- * 2). Considere base e altura números inteiros maior que 0.*/
-#include "stdio.h"
+#include <stdio.h>
+
 int main(void) {
-  
-  
-  int base = 1.0;
-  int altura = 1.0;
 
-  printf("Digite a base do triângulo:\n ");
-  scanf("%d", &base);
-  printf("Digite a altura do triângulo:\n ");
-  scanf("%d", &altura);
+  float base, altura;
 
-  float areaT = (base * altura / 2.0);
-  
-  
-  printf("A área do triângulo é: %.2f\n", areaT);
+  printf("Digite a base do triângulo: ");
+  scanf("%f", &base);
+
+  printf("Digite a altura do triângulo: ");
+  scanf("%f", &altura);
+
+  if (base <= 0 || altura <= 0) {
+    printf("A base e a altura do triângulo tem que ser maiores que 0.\n");
+    return 1; 
+  }
+
+  float area = base * altura / 2.0;
+
+  printf("A área do triângulo é: %.2f\n", area);
 
   return 0;
 }
