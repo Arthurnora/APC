f3434113e8aaafdda7788f31556a91aa42ea4f28
Arthur <138830298+Arthurnora@users.noreply.github.com>
2024-03-11 13:58:33 +0000

    .
    aula3
    pratica 3.6, 1.7

---
+++ .ccls-cache/@home@runner@APC/aulas@aula3@nota_legal.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int main() {
+  printf("|------------------------------------------------------|\n");
+  printf("|                                                      |\n");
+  printf("|                 N O T A  L E G A L\n                 |");
+  printf("|                                                      |\n");
+  printf("|------------------------------------------------------|\n");
+  printf("|Item                  Qtd                    Valor    |\n");
+
+  printf("%-23s %-21d %-9s\n", "|Caneta", 32,"49,90""R$|");
+  printf("%-25s %-21d %-9s\n", "|Maçã", 78,"10,00""R$|");
+  printf("%-23s %-21d %-9s\n", "|Batata", 14,"10,00""R$|");
+  printf("%-24s %-20d %-9s\n", "|Xuxu", 9,"10,00""R$|");
+  printf("%-24s %-20d %-9s\n", "|Repolho", 7,"10,00""R$|");
+  printf("%-24s %-20d %-9s\n", "|Batata doce", 3,"10,00""R$|");
+  printf("%-24s %-20d %-9s\n", "|Laranja", 7,"10,00""R$|");
+  printf("%-24s %-20d %-9s\n", "|Melancia", 5,"10,00""R$|");
+  printf("%-23s %-21d %-9s\n", "|Uva", 20,"44,00""R$|");
+  printf("%-23s %-21d %-9s\n", "|Morango", 50,"66,59""R$|");
+  printf("%-25s %-20d %-9s\n", "|Jiló", 5,"12,49""R$|");
+  printf("%-24s %-20d %-9s\n", "|Frango Caipira", 2,"15,99""R$|");
+  printf("%-24s %-20d %-9s\n", "|Pamonha", 1,"15,00""R$|");
+  printf("%-25s %-20d %-9s\n", "|Limão", 5,"2,00""R$ |");
+  printf("------------------------------------------------------\n");
+  return 0;
+}
---
+++ .ccls-cache/@home@runner@APC/praticas@pratica36@lista.c
@@ -0,0 +1,7 @@
+/*6. Faça um programa em C que liste todos os múltiplos de 3, entre 1 e 100.*/
+#include <stdio.h>
+
+int main(){
+
+  return 0;
+}
